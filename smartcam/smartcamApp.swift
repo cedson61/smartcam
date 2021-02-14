73c1836a63c305239b60a7407f55b13fc681bf81
Chase Edson <chase@cedson.cc>
2021-02-14 13:01:42 -0800

    Initial Commit

---
+++ smartcam/smartcamApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartcamApp.swift
+//  smartcam
+//
+//  Created by Chase Edson on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct smartcamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
